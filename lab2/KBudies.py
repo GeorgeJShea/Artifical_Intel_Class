469c58fe33eb21fbc34960cacaf5f459cb917965
George J Shea <52836004+GeorgeJShea@users.noreply.github.com>
2021-10-08 11:03:27 -0500

    Added comments
    removed some small unscerry fluff

--- lab2/KBudies.py
+++ lab2/KBudies.py
@@ -1,20 +1,18 @@
-import pandas as pd
-from sklearn.utils import shuffle
-from math import sqrt
-import numpy as np
-from matplotlib import pyplot as plt
-import time
-from collections import Counter
+import pandas as pd                         # Used for dataframes
+from sklearn.utils import shuffle           # Shuffles dataframe
+from math import sqrt                       # Knn calculation
+import numpy as np                          # Math calculation
+from matplotlib import pyplot as plt        # For plotting
+import time                                 # Used for calculating duration of calculation
+
 
 #________________________________________________________________________________________
 # Name: George Shea     ßeta
 # Date Created: 6/10/21
 # Date Modified 8/10/21
 # Lab 2 find knn for give data
-# Version 2.21
+# Version 2.3
 #________________________________________________________________________________________
-
-
 
 # put your path here
 path = ('C:/Users/gshea/Desktop/School/Summer2021/Artifical/DataSets/fruits_classification.xlsx')
@@ -26,15 +24,20 @@ if (extentsion == "xlsx"):
 if (extentsion == "csv"):
     dataFramePrime = pd.read_csv(path)
 
-def Primer(k=None, dataStart=None, dataEnd=None, classifer=None):
+def Primer(k=None, dataStart=None, dataEnd=None, classifer=None, split=None):
+    # Cuts out defined data
     endOf = dataFramePrime.iloc[:, dataStart:dataEnd+1]
+    # Runs normaliztion on defined data
     endOf = (endOf - endOf.mean()) / endOf.std()
+    # gets start of data
     startOF = dataFramePrime.iloc[:, :dataStart]
+    # Combinds and shuffles data
     result = startOF.join(endOf)
-    normalizedDf = shuffle(result)    #randomises data
+    normalizedDf = shuffle(result)
 
-    train = normalizedDf.sample(frac=0.6)     #splits best resutls apear to be closer to higher ratio of training data
-    test = normalizedDf.drop(train.index)     #splits this is due to the algrothem being "lazy"
+    # splits data at a 60/40 split
+    train = normalizedDf.sample(frac=split)
+    test = normalizedDf.drop(train.index)
 
     #Find friends up the ladder
     average = FindFriends(train, test, k, dataStart, dataEnd, classifer)
@@ -45,23 +48,19 @@ def FindFriends(train, test, k=None, dataStart=None, dataEnd=None, classifer=Non
     # 2 using knn makes a guess
     # 3 finnaly average correct guess verses total
 
-    friends = []
-    friendsIndex = []
-    topFriends = []
     testCounter = 0
     matchCount = 0
 
     # Goes through each item in test
     for index, rowTest in test.iterrows():
-        # Reset per indivual test
+        # Resets the following values for each instance of test
         topFriends = []
         friends = []
         friendsIndex = []
         trainCounter = 0
-        # and then does k nearest to each item in train data.
+        # Will go through each istance of test and do knn with all training data
         for index, rowTrain in train.iterrows():
-            # K nearest neighbor 3, 6 default for fruit
-            # start value is start of useable info and end is the final collum
+            # KNN function can be n length
             value = 0
             s = dataStart
             while s <= dataEnd:
@@ -71,37 +70,39 @@ def FindFriends(train, test, k=None, dataStart=None, dataEnd=None, classifer=Non
             # Adds neighbor and its index to the list
             friends.append(sqrt(value))
             friendsIndex.append((trainCounter))
+
             trainCounter = trainCounter + 1
 
-        # f is the nearest neighbor, fI is its index in the train data set
+        # F is the nearest neighbor value, fI is its index in the larger train data set.
         fI, f = np.array(friendsIndex), np.array(friends)
-        # Sorts index list based on nearest
+        # Sorts F into shortest to longest distance and than matchs fI to that organzation
         inds = f.argsort()
         fI, f = fI[inds], f[inds]
 
-        #Get x nearest neighbors ie top scores
+        #Gets top neighbors based on k
         i = 0
         while i <= k:
             topFriends.append(train.iloc[fI[i], classifer])
             i = i + 1
 
-        # Runs most frequent and returns most frequent in case of tie returns first encountered.
+        # Runs MostFrequent function will average the postion of same classifer and return the shorts average as its guess
         guess = MostFrequent(topFriends, f[:k])
 
-        #------------------------ change to 2
+        # If right + 1 else nothing
         if(test.iloc[testCounter, classifer] == guess):
             matchCount = matchCount + 1
 
         testCounter = testCounter + 1
-    # finnaly returns average
+
+    # finally returns average
     return round(matchCount / len(test) * 100, 2)
 
 def MostFrequent(listCount, fI):
-    # creates tuple of knn score and item
+    # Creates a tuple of f(knn distance) fI index
     mergedList = [(fI[i], listCount[i]) for i in range(0, len(fI))]
     output = {}
 
-    # Created dic and sorts by item
+    # Created dic and sorts by classifer
     for x, y in mergedList:
         if y in output:
             output[y].append((x, y))
@@ -109,31 +110,26 @@ def MostFrequent(listCount, fI):
             output[y] = [(x, y)]
 
     test = 0
-    counter = 0
     averageNearScore = []
+
     # Adds all same type knn scores together
     for key, value in output.items():
-        counter = 0
+        counter = 0 # dict index
         for x in value:
-            # addes knn scores together bases on same item
+            # Adds knn together based on classifer
             test = test + value[counter][0]
             counter = counter + 1
-        # averages knn scores from n items
+        # Average value based on n many items
         averageNearScore.append(test/len(value)/ len(value))
 
-    # gets lowests average as that is the collests items
-    # if there is a matching item somehow || it will take the istance of the first matching item from said set of matching items
+    # Gets lowest average score if there is a matching value it will return the first encountered item
     minnium = min(averageNearScore)
-    # gets dic keys index into based on index of shortest item
 
-    data = Counter(listCount)
-    test = data.most_common(1)[0][0]
-
+    # returns the classifer of the smallest average dict type
     return list(output.keys())[averageNearScore.index(minnium)]
 
-
 # Lets make toast used for running findfriends mutiple times.
-def ScreamingProcesser(iterations=None, kNear=None, startData=None, endData=None, classifer=None):
+def ScreamingProcesser(iterations=None, kNear=None, startData=None, endData=None, classifer=None, printVal=None, split=None):
     iterationsReset = iterations    # used for reset iterations
     kNearReset = kNear + 1          #Formatting
     averageList = []                #Stores averages for each knn
@@ -143,25 +139,27 @@ def ScreamingProcesser(iterations=None, kNear=None, startData=None, endData=None
         iterations = iterationsReset
         while iterations > 0:
             # 3  and 7 is what i want here -----------------------------------
-            temp = Primer(kNear, startData, endData, classifer)
+            temp = Primer(kNear, startData, endData, classifer, split)
             runningTotal = runningTotal + temp
             iterations = iterations - 1
 
         averageList.append(round(runningTotal / iterationsReset, 2))
-        print("|| Neighbors || ", kNear, " || Average Accuracy || " ,round(runningTotal / iterationsReset, 2), "%")
+        if(printVal == True):
+            print("|| Neighbors || ", kNear, " || Average Accuracy || " ,round(runningTotal / iterationsReset, 2), "%")
         kNear = kNear - 1
 
-    # Plot stuff
-    averageList = averageList[::-1]
-    averageList = [0] + averageList
-    plt.bar(range(kNearReset), averageList)
-    plt.xticks(range(0, kNearReset))
-    plt.title("K Nearest Neighbors")
-    plt.xlabel("How Many Neighbors")
-    plt.ylabel("Precents")
-    plt.show()
+    if(printVal == True):
+        # Plot stuff
+        averageList = averageList[::-1]
+        averageList = [0] + averageList
+        plt.bar(range(kNearReset), averageList)
+        plt.xticks(range(0, kNearReset))
+        plt.title("K Nearest Neighbors")
+        plt.xlabel("How Many Neighbors")
+        plt.ylabel("Precents")
+        plt.show()
 
-    return averageList
+    return averageList[1:len(averageList)]
 
 def main():
     now = time.time()
@@ -169,7 +167,7 @@ def main():
     value =  int(input("Please enter iteration: \n    "))
     print("Loading... . ")
     print("_________________________________________________________")
-    averageList = ScreamingProcesser(iterations=value, classifer=1, startData=3, endData=6, kNear=10)
+    averageList = ScreamingProcesser(iterations=value, classifer=1, startData=3, endData=6, kNear=10, split=.6, printVal=True)
     print("_________________________________________________________")
     index = max(averageList)
     print("Most Accurate KNN")
@@ -177,4 +175,7 @@ def main():
     print("_________________________________________________________")
     later = time.time()
     print("Complete ||    || Time elasped || ", round((later - now)/60, 2), " Minutes")
-main()
+
+
+if __name__ == '__main__':
+    main()
